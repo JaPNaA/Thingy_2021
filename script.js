ab175b38b7086d353f7fa42de441562d5a88b9c0
2 1 <>
2021-05-03 15:31:18 +0000

    Copy in elements.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-/** @type {HTMLCanvasElement} */
-const canvas = document.getElementById("canvas");
-const X = canvas.getContext("2d");
+class Canvas {
+    constructor() {
+        this.elm = document.createElement("canvas");
+        this.X = this.elm.getContext("2d");
+    }
 
-X.fillRect(10, 10, 40, 40);
+    resizeToScreen() {
+        const scaling = window.devicePixelRatio || 1;
+        this.elm.width = innerWidth * scaling;
+        this.elm.height = innerHeight * scaling;
+    }
+}
+
+class UserInterface {
+    constructor() {
+        this.elm = document.createElement("div");
+        this.canvas = new Canvas();
+    }
+
+    onResize() {
+        this.canvas.resizeToScreen();
+    }
+}
+
+const ui = new UserInterface();
+
+ui.onResize();
+
+addEventListener("resize", () => ui.onResize());
+
+ui.canvas.X.fillRect(0, 0, 100, 100);
+
+
