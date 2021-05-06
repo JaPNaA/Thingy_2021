5b6327cba45b4b53970d6484adba4f73ac5120a6
21 <>
2021-05-06 18:12:43 +0000

    Use HitBox for cursor interaction

--- ui/canvas/vectorInput/VectorLinearInput.js
+++ ui/canvas/vectorInput/VectorLinearInput.js
@@ -1,5 +1,6 @@
 import { vec, Vec2 } from "../../../utils/vectors.js";
 import { CanvasElm } from "../CanvasElm.js";
+import { HitBox } from "../HitBox.js";
 
 export class VectorLinearInput extends CanvasElm {
     /**
@@ -12,26 +13,49 @@ export class VectorLinearInput extends CanvasElm {
         this.direction = direction;
         this.tailPos = tailPos;
         this.valueVector = this.direction;
+
+        this.dragging = false;
+
+        this.hitbox = new HitBox(this._getHitboxCorner(), vec(4, 4));
+        this.hitbox.setMousedownHandler(() => this.mousedownHandler());
+    }
+
+    /** @param {import("./World.js").World} world */
+    setup(world) {
+        super.setup(world);
+        world.addHitbox(this.hitbox);
     }
 
     draw() {
-        this.magnitude = this.world.cursor.x;
-        this.valueVector = this.getVec2();
+        if (this.dragging) {
+            this.magnitude = this.world.cursor.subtract(this.tailPos).dot(this.direction) / this.direction.magnitude; 
+            this.valueVector = this.getVec2();
+        }
 
         const canvas = this.world.canvas;
-        const headX = this.tailPos.x + this.valueVector.x;
-        const headY = this.tailPos.y + this.valueVector.y;
+        const headPos = this.tailPos.add(this.valueVector);
 
         canvas.X.strokeStyle = "#ffffff";
         canvas.X.fillStyle = "#ff0000";
         canvas.X.beginPath();
         canvas.X.moveTo(this.tailPos.x, this.tailPos.y);
-        canvas.X.lineTo(headX, headY);
+        canvas.X.lineTo(headPos.x, headPos.y);
         canvas.X.stroke();
-        canvas.X.fillRect(headX - 2, headY - 2, 4, 4);
+        canvas.X.fillRect(headPos.x - 2, headPos.y - 2, 4, 4);
+
+        this.hitbox.setPos(this._getHitboxCorner());
+    }
+
+    mousedownHandler() {
+        this.dragging = true;
+        this.world.cursor.nextMouseUp().then(() => this.dragging = false);
     }
 
     getVec2() {
         return this.direction.withMagnitude(this.magnitude);
+    }
+
+    _getHitboxCorner() {
+        return this.tailPos.add(this.valueVector).subtract(vec(2, 2));
     }
 }
