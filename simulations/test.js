e94565520ff097e1195e959015d2bc9ca383219d
2 1 <>
2021-05-11 14:16:02 +0000

    Cover window with HTMLCanvas

--- simulations/test.js
+++ simulations/test.js
@@ -1,11 +1,20 @@
 // Testing grounds. Not an actual simulation
 
 import { HTMLCanvasElm } from "../ui/htmlCanvas/HTMLCanvasElm.js";
+import { Elm, InputElm } from "../utils/elements.js";
+
+class HTMLCanvasInputElm extends HTMLCanvasElm {
+    constructor() {
+        super();
+
+        this.append(new InputElm());
+    }
+}
 
 /** @param {SimulationView} simulationView */
 export function start(simulationView) {
     simulationView.htmlCanvas.addElm(
-        new HTMLCanvasElm()
+        new HTMLCanvasInputElm()
     );
 }
 
