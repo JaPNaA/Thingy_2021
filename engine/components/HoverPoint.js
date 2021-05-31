34d5611eb2bdd7a09108c4e73e0a006f96b9d79c
2 1 <>
2021-05-31 00:52:41 +0000

    Move Ball to own file as HoverPoint

---
+++ engine/components/HoverPoint.js
@@ -0,0 +1,46 @@
+import { vec } from "/utils/vectors.js";
+import { CanvasElm } from "../canvas/CanvasElm.js";
+import { HitBox } from "../canvas/HitBox.js";
+
+export class HoverPoint extends CanvasElm {
+    constructor() {
+        super();
+        
+        this.offset = vec(0, 0);
+        this.pos = vec(0, 0);
+
+        this.hovering = false;
+
+        this.hitbox = new HitBox(this._getHitboxCorner(), vec(8, 8));
+        this.hitbox.setMousemoveHandler(() => this.hovering = true);
+        this.hitbox.setMouseoffHandler(() => this.hovering = false);
+    }
+
+    setup(world) {
+        super.setup(world);
+        world.addHitbox(this.hitbox);
+    }
+
+    draw() {
+        const X = this.world.canvas.X;
+
+        const {x, y} = this.pos.add(this.offset);
+
+        X.fillStyle = "#ff0000";
+
+        X.beginPath();
+        X.arc(x, y, 4, 0, 2 * Math.PI);
+        X.closePath();
+        X.fill();
+
+        this.hitbox.setPos(this._getHitboxCorner());
+
+        if (this.hovering) {
+            X.fillText(this.pos.toString(2), x, y);
+        }
+    }
+
+    _getHitboxCorner() {
+        return this.pos.add(this.offset).subtract(vec(4, 4));
+    }
+}
--- simulations/projectileMotion.js
+++ simulations/projectileMotion.js
@@ -3,8 +3,7 @@ import { World } from "../engine/World.js";
 import { VectorLinearInput } from "../engine/components/vectorInput/VectorLinearInput.js";
 import { VectorInput } from "../engine/components/vectorInput/VectorInput.js";
 import { TimePath } from "../engine/components/timePath/TimePath.js";
-import { CanvasElm } from "../engine/canvas/CanvasElm.js";
-import { HitBox } from "../engine/canvas/HitBox.js";
+import { HoverPoint } from "../engine/components/HoverPoint.js";
 
 const equasions = {
     /**
@@ -31,50 +30,10 @@ const equasions = {
     y: (t, g, theta, v, y0) => v * Math.sin(theta) * t  - g * (t * t) / 2 + y0
 };
 
-class Ball extends CanvasElm {
-    constructor() {
-        super();
-        
-        this.pos = vec(0, 0);
-
-        this.hovering = false;
-
-        this.hitbox = new HitBox(this._getHitboxCorner(), vec(8, 8));
-        this.hitbox.setMousemoveHandler(() => this.hovering = true);
-        this.hitbox.setMouseoffHandler(() => this.hovering = false);
-    }
-
-    setup(world) {
-        super.setup(world);
-        world.addHitbox(this.hitbox);
-    }
-
-    draw() {
-        const X = this.world.canvas.X;
-        
-        X.fillStyle = "#ff0000";
-
-        X.beginPath();
-        X.arc(this.pos.x, this.pos.y, 4, 0, 2 * Math.PI);
-        X.closePath();
-        X.fill();
-
-        this.hitbox.setPos(this._getHitboxCorner());
-
-        if (this.hovering) {
-            X.fillText(this.pos, this.pos.x, this.pos.y);
-        }
-    }
-
-    _getHitboxCorner() {
-        return this.pos.subtract(vec(4, 4));
-    }
-}
-
 /** @type {import("../ui/canvas/World.js").World} */
 let world;
 
-const ball = new Ball();
+const ball = new HoverPoint();
 
 const vInput = new VectorInput(vec(10, -10), vec(10, 400));
 const timeInput = new VectorLinearInput(vec(10, 0), vec(10, 10));
@@ -123,8 +82,10 @@ export function update(timeElapsed) {
 
     const time = timeInput.getMagnitude();
 
-    ball.pos = getPositionAtTime(time);
-    vInput.setTailPos(initialPositionInput.getVec2().add(initialPositionInput.getTailPos()));
+    const baseOffset = initialPositionInput.getTailPos();
+    ball.pos = getPositionAtTime(time).subtract(baseOffset);
+    ball.offset = baseOffset;
+    vInput.setTailPos(initialPositionInput.getVec2().add(baseOffset));
 
     world.draw();
 }
