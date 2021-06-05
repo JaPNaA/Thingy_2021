5b2b80980b58234faadad1c65ab11ff66ba514fd
2 1 <>
2021-06-05 23:41:26 +0000

    Draw triangles instead of squares

--- engine/components/VectorArrow.js
+++ engine/components/VectorArrow.js
@@ -7,6 +7,7 @@ export class VectorArrow extends CanvasElm {
         this.tailPos = tail;
         this.value = value;
         this.tailHightlighted = false;
+        this.arrowSize = 8;
     }
 
     draw() {
@@ -15,15 +16,22 @@ export class VectorArrow extends CanvasElm {
 
         const relTailPos = this.world.camera.transformPoint(this.tailPos);
         const relHeadPos = this.world.camera.transformPoint(this.tailPos.add(this.value));
+        const angle = this.value.angle + Math.PI / 4;
 
         X.strokeStyle = "#ffffff";
         X.lineWidth = 1;
-        X.fillStyle = this.tailHightlighted ? "#ff0000" : "#aaaaaa";
         X.beginPath();
         X.moveTo(relTailPos.x, relTailPos.y);
         X.lineTo(relHeadPos.x, relHeadPos.y);
         X.stroke();
-        X.fillRect(relHeadPos.x - 2, relHeadPos.y - 2, 4, 4);
+
+        X.fillStyle = this.tailHightlighted ? "#ff0000" : "#aaaaaa";
+        X.beginPath();
+        X.moveTo(relHeadPos.x, relHeadPos.y);
+        X.lineTo(relHeadPos.x - this.arrowSize * Math.sin(angle), relHeadPos.y + this.arrowSize * Math.cos(angle));
+        X.lineTo(relHeadPos.x - this.arrowSize * Math.cos(angle), relHeadPos.y - this.arrowSize * Math.sin(angle));
+        X.closePath();
+        X.fill();
     }
 
     setTail(tail) {
