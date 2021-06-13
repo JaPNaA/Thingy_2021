062bceef7cb91000e0fdbf5a189ff72d6f1d9619
Roxasbtg <>
2021-06-13 00:45:10 +0000

    Completed inputting collision equations

--- simulations/1dCollisions.js
+++ simulations/1dCollisions.js
@@ -1,32 +1,68 @@
-import {vec} from "../utils/vectors.js";
-import {VectorLinearInput} from "../engine/components/vectorInput/VectorLinearInput.js";
-import {World} from "../engine/World.js";
+import { vec } from "../utils/vectors.js";
+import { VectorLinearInput } from "../engine/components/vectorInput/VectorLinearInput.js";
+import { World } from "../engine/World.js";
 import { ExpressionSolver } from "../engine/components/expressionSolver/ExpressionSolver.js";
 import { CanvasElm } from "../engine/canvas/CanvasElm.js";
 import { Elm } from "../utils/elements.js";
 import { HTMLCanvasElm } from "../engine/htmlCanvas/HTMLCanvasElm.js";
-/*
-const equations{
 
-}
+const equations = {
+
+    vf1: (m1, m2, vi1, vi2) => (
+        ((m1 - m2) / (m1 + m2)) * vi1 +
+        ((2 * m2) / (m1 + m2)) * vi2
+    ),
+
+    vf2: (m1, m2, vi1, vi2) => (
+        ((2 * m2) / (m1 + m2)) * vi1 + 
+        ((m1 - m2) / (m1 + m2)) * vi2
+    )
+
+};
 
 //User Inputs
 const velocity1Input = new VectorLinearInput(vec(), vec()); //Position not determined yet
-velocityInput.setUnitText("m/s");
+velocity1Input.setUnitText("m/s");
 
 const mass1Input = new VectorLinearInput(vec(), vec()); //Position not determined yet
-velocityInput.setUnitText("kg");
+mass1Input.setUnitText("kg");
 
 const velocity2Input = new VectorLinearInput(vec(), vec()); //Position not determined yet
-velocityInput.setUnitText("m/s");
+velocity2Input.setUnitText("m/s");
 
 const mass2Input = new VectorLinearInput(vec(), vec()); //Position not determined yet
-velocityInput.setUnitText("m/s");
+mass2Input.setUnitText("m/s");
 
-export function start(something){
-    world = new world(something);
+
+class CollisionDraw extends CanvasElm {
+    
+    constructor(){
+        
+        super();
+
+        this.pos = vec(0, 0);
+    }
+
+    draw(){
+        /** @type {CanvasRenderingContext2D} */
+        const X = this.world.canvas.X;
+
+        X.beginPath
+        X.fillStyle = "#ff0000";
+        X.fill();
+    }
 }
 
-export function stop(){
+let world;
+
+export function start(simulationView) {
+    world = new World(simulationView);
+}
+
+export function stop() {
     world.setdown();
+}
+
+export function update(timeElapsed) {
+    world.draw();
 }
