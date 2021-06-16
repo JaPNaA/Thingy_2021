8cd029df8390f11efe369df4ee291f3ac16a15d4
2 1 <>
2021-06-16 23:47:30 +0000

    Draw a scale in grid

--- engine/components/Grid.js
+++ engine/components/Grid.js
@@ -1,9 +1,13 @@
 import { CanvasElm } from "../canvas/CanvasElm.js";
+import { vec } from "/utils/vectors.js";
+import { colors } from "/ui/colors.js";
 
 export class Grid extends CanvasElm {
     constructor() {
         super();
         this.staticPosition = true;
+
+        this.drawScale = true;
     }
 
     draw() {
@@ -11,21 +15,15 @@ export class Grid extends CanvasElm {
         const height = this.world.canvas.height;
         this._drawGridCellSize(1000, width, height);
         this._drawGridCellSize(100, width, height);
+        this._drawScale();
     }
 
     _drawGridCellSize(scaleSize, gridWidth, gridHeight) {
         /** @type {CanvasRenderingContext2D} */
         const X = this.world.canvas.X;
 
-        const cellSize =
-            scaleSize * this.world.camera.zoom * (
-                Math.pow(
-                    10,
-                    Math.floor(
-                        -Math.log10(this.world.camera.zoom)
-                    )
-                )
-            );
+        const cellSize = this._calculateCellSize(scaleSize);
+            
         const offsetX = -this.world.camera.x % cellSize;
         const offsetY = -this.world.camera.y % cellSize;
 
@@ -48,5 +46,41 @@ export class Grid extends CanvasElm {
 
         X.stroke();
         X.restore();
+    }
+
+    _calculateCellSize(scaleSize) {
+        return scaleSize * this.world.camera.zoom * (
+            Math.pow(
+                10,
+                Math.floor(
+                    -Math.log10(this.world.camera.zoom)
+                )
+            )
+        );
+    }
+
+    _drawScale() {
+        // const bottomLeft = vec(0, this.world.canvas.height);
+
+        /** @type {CanvasRenderingContext2D} */
+        const X = this.world.canvas.X;
+
+        const bottomLeft = vec(8, this.world.canvas.height - 8);
+
+        let cellSize = this._calculateCellSize(100);
+        if (cellSize < 32) {
+            cellSize = this._calculateCellSize(1000);
+        }
+
+        X.lineWidth = 2;
+        X.strokeStyle = colors.white;
+        X.beginPath();
+        X.moveTo(bottomLeft.x, bottomLeft.y);
+        X.lineTo(bottomLeft.x + cellSize, bottomLeft.y);
+        X.stroke();
+
+        X.textAlign = "center";
+        X.fillStyle = colors.white;
+        X.fillText(Number((cellSize / this.world.camera.zoom).toPrecision(1)) + "m", bottomLeft.x + cellSize / 2, bottomLeft.y - 8);
     }
 }
