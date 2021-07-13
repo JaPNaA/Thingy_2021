403fec06df27eb616dc2603689f08c9899657548
JaPNaA <leonehuang100@gmail.com>
2021-07-13 13:40:26 -0400

    Split MapEditor and MapEditorOverlay files

--- Culsion/src/view/mapEditor/MapEditor.ts
+++ Culsion/src/view/mapEditor/MapEditor.ts
@@ -1,11 +1,11 @@
-import { Component, Elm } from "../../engine/elements";
 import { ParentCanvasElm } from "../../engine/ParentCanvasElm";
 import { World } from "../../engine/World";
 import { GhostPlayer } from "../../entities/GhostPlayer";
 import { TileMap } from "../../entities/TileMap";
 import { resourceFetcher } from "../../resources/resourceFetcher";
-import { BlockType, TileMapFile } from "../../resources/TileMapFile";
+import { TileMapFile } from "../../resources/TileMapFile";
 import { settings } from "../../settings";
+import { MapEditorOverlay } from "./MapEditorOverlay";
 
 export class MapEditor extends ParentCanvasElm {
     private tileMap?: TileMap;
@@ -93,78 +93,5 @@ export class MapEditor extends ParentCanvasElm {
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
-    }
-}
-
-class MapEditorOverlay extends Component {
-    public selectedBlock = 1;
-
-    private blockTypeElms: Elm<any>[] = [];
-
-    private blockTypesElm = new Elm().class("blockTypes")
-        .appendTo(this.elm);
-    private canvasSizeElm = new Elm().class("canvasSize")
-        .on("click", () => this.openChangeMapSizeDialog())
-        .appendTo(this.elm);
-
-    private tileMap?: TileMap;
-
-    constructor() {
-        super("MapEditorOverlay");
-    }
-
-    public setTileMap(tileMap: TileMap) {
-        this.tileMap = tileMap;
-
-        this.setBlockTypes(tileMap.getBlockTypes());
-        this.setCanvasSize(tileMap.getWidth(), tileMap.getHeight());
-    }
-
-    private setBlockTypes(blockTypes: readonly BlockType[]) {
-        for (let i = 0; i < blockTypes.length; i++) {
-            const blockType = blockTypes[i];
-
-            let elm: Elm<any>;
-
-            if (blockType.texture) {
-                elm = new Elm("img").attribute("src", "assets/img/tile/" + blockType.texture + ".png");
-            } else {
-                elm = new Elm("div");
-            }
-
-            elm.class("blockType").attribute("style", "background-color: " + blockType.color);
-
-            elm.on("click", () => {
-                this.selectBlock(i);
-            });
-
-            this.blockTypesElm.append(elm);
-            this.blockTypeElms[i] = elm;
-        }
-
-        this.selectBlock(this.selectedBlock);
-    }
-
-    private selectBlock(index: number) {
-        this.blockTypeElms[this.selectedBlock].removeClass("selected");
-        this.selectedBlock = index;
-        this.blockTypeElms[index].class("selected");
-    }
-
-    private setCanvasSize(width: number, height: number) {
-        this.canvasSizeElm.replaceContents(width + "x" + height);
-    }
-
-    private openChangeMapSizeDialog() {
-        if (!this.tileMap) { return; }
-
-        //* temporary
-        const newWidth = parseInt(prompt("New width")!);
-        if (isNaN(newWidth)) { return; }
-        const newHeight = parseInt(prompt("New height")!);
-        if (isNaN(newHeight)) { return; }
-
-        this.tileMap.resize(newWidth, newHeight);
-        this.setCanvasSize(newWidth, newHeight);
     }
 }
---
+++ Culsion/src/view/mapEditor/MapEditorOverlay.ts
@@ -0,0 +1,76 @@
+import { Component, Elm } from "../../engine/elements";
+import { TileMap } from "../../entities/TileMap";
+import { BlockType } from "../../resources/TileMapFile";
+
+export class MapEditorOverlay extends Component {
+    public selectedBlock = 1;
+
+    private blockTypeElms: Elm<any>[] = [];
+
+    private blockTypesElm = new Elm().class("blockTypes")
+        .appendTo(this.elm);
+    private canvasSizeElm = new Elm().class("canvasSize")
+        .on("click", () => this.openChangeMapSizeDialog())
+        .appendTo(this.elm);
+
+    private tileMap?: TileMap;
+
+    constructor() {
+        super("MapEditorOverlay");
+    }
+
+    public setTileMap(tileMap: TileMap) {
+        this.tileMap = tileMap;
+
+        this.setBlockTypes(tileMap.getBlockTypes());
+        this.setCanvasSize(tileMap.getWidth(), tileMap.getHeight());
+    }
+
+    private setBlockTypes(blockTypes: readonly BlockType[]) {
+        for (let i = 0; i < blockTypes.length; i++) {
+            const blockType = blockTypes[i];
+
+            let elm: Elm<any>;
+
+            if (blockType.texture) {
+                elm = new Elm("img").attribute("src", "assets/img/tile/" + blockType.texture + ".png");
+            } else {
+                elm = new Elm("div");
+            }
+
+            elm.class("blockType").attribute("style", "background-color: " + blockType.color);
+
+            elm.on("click", () => {
+                this.selectBlock(i);
+            });
+
+            this.blockTypesElm.append(elm);
+            this.blockTypeElms[i] = elm;
+        }
+
+        this.selectBlock(this.selectedBlock);
+    }
+
+    private selectBlock(index: number) {
+        this.blockTypeElms[this.selectedBlock].removeClass("selected");
+        this.selectedBlock = index;
+        this.blockTypeElms[index].class("selected");
+    }
+
+    private setCanvasSize(width: number, height: number) {
+        this.canvasSizeElm.replaceContents(width + "x" + height);
+    }
+
+    private openChangeMapSizeDialog() {
+        if (!this.tileMap) { return; }
+
+        //* temporary
+        const newWidth = parseInt(prompt("New width")!);
+        if (isNaN(newWidth)) { return; }
+        const newHeight = parseInt(prompt("New height")!);
+        if (isNaN(newHeight)) { return; }
+
+        this.tileMap.resize(newWidth, newHeight);
+        this.setCanvasSize(newWidth, newHeight);
+    }
+}
