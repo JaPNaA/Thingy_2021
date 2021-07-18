55ce18ca675edcfc9351bffaf8940da445259d7e
JaPNaA <leonehuang100@gmail.com>
2021-07-17 21:10:14 -0400

    Move faster when zoomed out

--- Culsion/src/entities/GhostPlayer.ts
+++ Culsion/src/entities/GhostPlayer.ts
@@ -3,4 +3,10 @@ import { Player } from "./Player";
 
 export class GhostPlayer extends Player {
     public collisionType = collisions.types.none;
+    public actualSpeed = 1000;
+
+    public tick() {
+        this.speed = this.actualSpeed / this.world.camera.scale;
+        super.tick();
+    }
 }
